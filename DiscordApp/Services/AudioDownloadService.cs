909f2f773a0a449b5aa75fb057ce58048a9a8007
skyeelove <staszhuk2017@gmail.com>
2025-07-09 11:24:14 +0200

    (feature) search  by title, expected time of search +-7 sec(slow)

---
+++ DiscordApp/Services/AudioDownloadService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace DiscordApp.Services
+{
+    public static class AudioDownloadService
+    {
+        public static async Task<Song?> GetAudioDataAsync(string queryOrUrl)
+        {
+            string Arguments = string.Empty;
+            if (IsValidUrl(queryOrUrl) == false)
+            {
+                Arguments = $"--yes-playlist -f bestaudio --get-title --get-url ytsearch1:\"{queryOrUrl}\"";
+            }
+            else
+            {
+                Arguments = $"--yes-playlist -f bestaudio --get-title -g \"{queryOrUrl}\"";
+            }
+            var psi = new ProcessStartInfo
+                {
+                    FileName = "yt-dlp",
+                    Arguments = Arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+            using var process = new Process { StartInfo = psi, EnableRaisingEvents = true };
+            var output = new List<string>();
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (e.Data == null)
+                    tcs.TrySetResult(true);
+                else
+                    output.Add(e.Data);
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+
+            await tcs.Task;
+            await process.WaitForExitAsync();
+
+            string title = output.Count > 0 ? output[0] : "Unknown title";
+            string url = output.Count > 1 ? output[1] : "";
+
+            return new Song(title, url);
+        }
+
+        private static bool IsValidUrl(string input)
+        {
+            return Uri.TryCreate(input, UriKind.Absolute, out Uri uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
